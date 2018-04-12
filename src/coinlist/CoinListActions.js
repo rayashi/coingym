ffc13149fc4ede23706ecbb494dc4975892667c7
Humberto Rayashi <rayashi@gmail.com>
2018-04-12 01:06:31 +0000

    Merged in improve-getting-prices (pull request #25)
    Improve getting prices
    
    * Request coinmarketcap just once at CoinList
    
    * Fixing getting prices
    
    * Increment version
    
    * Increment version
    
    * Update market list just once
    
    Approved-by: Jayr Motta <jayrmotta@gmail.com>

--- src/coinlist/CoinListActions.js
+++ src/coinlist/CoinListActions.js
@@ -1,6 +1,8 @@
 import axios from 'axios'
 import firebase from 'react-native-firebase'
 
+const PRICES_LIMIT = 300
+
 export const setMarkets = (value) => {
   return {type: 'set_markets', payload: value}
 }
@@ -17,44 +19,29 @@ export const getMarkets = (funds, fundToSell) => {
     snapshot.forEach(async doc => {
       markets.push({ ...doc.data(), id: doc.id, loadingPrice: true})
     })
-    dispatch({ type: 'add_markets', payload: markets })
-    dispatch({ type: 'loading_markets', payload: false })
-    getMarketPrices(markets, funds, dispatch)
+    loadMarketPrices(markets, funds,  dispatch)
   }
 }
 
-const getMarketPrices = (markets, funds, dispatch) => {
-  markets.map( async market => {
-    market.base.ticker = { price_usd: 1 }
-    market.quote.ticker = { price_usd: 1 }
-    if(market.base.id !== 'usd'){
-      try{
-        let instance = axios.create({
-          baseURL: `https://api.coinmarketcap.com/v1/ticker/${market.base.name}/`
-        })
-        let result = await instance.get()
-        market.base.ticker = result.data[0]
-      }catch(error) {
-        console.log(`Error on ticker: ${market.base.name}, error: ${error}`)
-      }
-    }
-    if(market.quote.id !== 'usd'){
-      try{
-        let instance = axios.create({
-          baseURL: `https://api.coinmarketcap.com/v1/ticker/${market.quote.name}/`
-        })
-        let result = await instance.get()
-        market.quote.ticker = result.data[0]
-      }catch(error) {
-        console.log(`Error on ticker: ${market.quote.name}, error: ${error}`)
-      }
-    }
-    market.price = market.base.ticker.price_usd / market.quote.ticker.price_usd
-    market.price = market.price.toFixed(market.quote.precision)
-    market.loadingPrice = false
+const loadMarketPrices = async (markets, funds, dispatch) => {
+  const prices = listToObject(await getMarketPrices())
+  if(!prices) return null
+  markets.map( async market => { 
+    market.base.ticker = getTicker(market.base.id, prices)
+    market.quote.ticker = getTicker(market.quote.id, prices)
+    market.price = calculateMarketPrice(market)
     market.available = getMarketAvailability(market, funds)
-    dispatch({ type: 'update_market', payload: market })
+    market.loadingPrice = false
   })
+  markets.filter( market => !market.price  )
+  dispatch({ type: 'add_markets', payload: markets })
+  dispatch({ type: 'loading_markets', payload: false })
+}
+
+const calculateMarketPrice = (market) => {
+  if(!market.base.ticker || !market.quote.ticker) return 0
+  let price = market.base.ticker.price_usd / market.quote.ticker.price_usd
+  return Number(price.toFixed(market.quote.precision))
 }
 
 const getMarketAvailability = (market, funds) =>{
@@ -62,4 +49,24 @@ const getMarketAvailability = (market, funds) =>{
   if(fund && !fund.pending && (fund.amount - (fund.amountInOrder||0)) > 0) 
     return true
   return false
+}
+
+const getMarketPrices = async () => {
+  let instance = axios.create({ baseURL: 'https://api.coinmarketcap.com/v1/ticker/' })
+  try {
+    return (await instance.get('/', {params: { limit: PRICES_LIMIT }})).data
+  } catch (error) {
+    return false
+  }
+}
+
+const getTicker = (marketId, prices) => {
+  if (marketId === 'usd') return { price_usd: 1 }
+  return prices[marketId.toUpperCase()]
+}
+
+const listToObject = list => {
+  let obj = {}
+  list.map(x => { obj[x.symbol] = x })
+  return obj
 }
